5e96f08b766abdedbdd56a5eb9a1d002166bdf92
Eric Colton <sderic76@gmail.com>
2020-06-11 08:32:24 -0400

    fixed 07 tests

--- 2019/aoc_2019_07/test_aoc_2019_07.py
+++ 2019/aoc_2019_07/test_aoc_2019_07.py
@@ -19,12 +19,12 @@ def test_example_program_3():
 
 # Feedback mode tests
 
-def test_feedback_program_4():
+def test_example_feedback_program_4():
     program = parse_program_str("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5")
     rv = find_max_output_all_perms(program, set(range(5, 10)))
     assert rv == 139629729
 
-def test_feedback_program_5():
+def test_example_feedback_program_5():
     program = parse_program_str("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10")
     rv = find_max_output_all_perms(program, set(range(5, 10)))
     assert rv == 18216
@@ -33,3 +33,5 @@ if __name__ == '__main__':
     test_example_program_1()
     test_example_program_2()
     test_example_program_3()
+    test_example_feedback_program_4()
+    test_example_feedback_program_5()
