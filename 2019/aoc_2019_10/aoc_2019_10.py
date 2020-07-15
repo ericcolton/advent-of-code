dddbefb62e3283191478f872125ad6f39b1bda7e
Eric Colton <sderic76@gmail.com>
2020-07-15 11:04:01 -0400

    added 2019 10 solution

--- 2019/aoc_2019_10/aoc_2019_10.py
+++ 2019/aoc_2019_10/aoc_2019_10.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+"""
+Advent of Code 2019 Day 10: Monitoring Station
+
+https://adventofcode.com/2019/day/10
+"""
+
 import math
 from collections import defaultdict
 from functools import reduce
@@ -11,14 +17,14 @@ def get_relative_angle(origin: (int, int), candidate: (int, int)) -> float:
     cand_x, cand_y = candidate
     rel_x, rel_y = cand_x - origin_x, cand_y - origin_y
     return round(math.atan2(rel_y, rel_x), ANGLE_TOLERANCE_DIGITS)
-    
+
 def bucket_peers_by_angle(origin: (int, int), peers: [(int, int)]) -> dict:
     peers_by_angle = defaultdict(list)
     for peer in peers:
         if origin != peer:
             peers_by_angle[get_relative_angle(origin, peer)].append(peer)
     return peers_by_angle
-        
+
 def find_max_observables(input: [(int, int)]) -> int:
     max_observables_count = 0
     max_observables_location = 0, 0
@@ -30,20 +36,19 @@ def find_max_observables(input: [(int, int)]) -> int:
     return max_observables_count, max_observables_location
 
 def angle_sort_key(angle):
-    if angle > math.pi / 2:
-        return angle - 2 * math.pi
+    if angle < -math.pi / 2:
+        return angle + 2 * math.pi
     return angle
 
 def find_destory_order(location: (int, int), peers: [(int, int)]) -> list:
-    peers_by_angle = bucket_peers_by_angle(location, all_nodes)
-    max_rotations = 0
+    peers_by_angle = bucket_peers_by_angle(location, peers)
     for peers_at_angle in peers_by_angle.values():
-        peers_at_angle.sort(key=lambda peer: peer[0] - location[0] + peer[1] - location[1])
-        max_rotations = max(max_rotations, len(peers_at_angle))
+        peers_at_angle.sort(key=lambda peer: abs(peer[0] - location[0]) + abs(peer[1] - location[1]))
 
+    max_rotations = max([len(i) for i in peers_by_angle.values()])
     destroyed = []
     for _ in range(max_rotations):
-        for angle in sorted(peers_by_angle.keys(), key=angle_sort_key, reverse=True):
+        for angle in sorted(peers_by_angle.keys(), key=angle_sort_key):
             peers_at_angle = peers_by_angle[angle]
             if len(peers_at_angle) > 0:
                 destroyed.append(peers_at_angle.pop(0))
@@ -58,8 +63,6 @@ def parse_input_data(input: str):
                 nodes.append((x, y))
     return nodes
 
-
-    
 if __name__ == '__main__':
     input_filename = __file__.rstrip('.py') + '_input.txt'
     with open(input_filename, 'r') as file:
@@ -70,5 +73,6 @@ if __name__ == '__main__':
     assert part_1 == 347
 
     destroy_order = find_destory_order(location, all_nodes)
-    part_2 = destroy_order[200]
+    part_2 = destroy_order[199] # 200th entry
     print(f"Solution to part 2 is {part_2}")
+    assert part_2[0] * 100 + part_2[1] == 829
--- 2019/aoc_2019_10/test_aoc_2019_10.py
+++ 2019/aoc_2019_10/test_aoc_2019_10.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import re
-from aoc_2019_10 import parse_input_data, find_max_observables
+from aoc_2019_10 import parse_input_data, find_max_observables, find_destory_order
 
 def test_case_one():
     test_data = '''......#.#.
@@ -89,5 +89,50 @@ if __name__ == '__main__':
     for symbol in dir():
         if re.search('^test_', symbol):
             print("Running {symbol}()")
+            globals()[symbol]()
+    print("Done")
+
+
+def test_case_four_destroy_order():
+    test_data = '''.#..##.###...#######
+##.############..##.
+.#.######.########.#
+.###.#######.####.#.
+#####.##.#.##.###.##
+..#####..#.#########
+####################
+#.####....###.#.#.##
+##.#################
+#####.##.###..####..
+..######..##.#######
+####.##.####...##..#
+.#####..#.######.###
+##...#.##########...
+#.##########.#######
+.####.#.###.###.#.##
+....##.##.###..#####
+.#.#.###########.###
+#.#.#.#####.####.###
+###.##.####.##.#..##'''
+    input = parse_input_data(test_data)
+    location = (11, 13)
+    destroy_order = find_destory_order(location, input)
+
+    assert destroy_order[0] == (11, 12)
+    assert destroy_order[1] == (12, 1)
+    assert destroy_order[2] == (12, 2)
+    assert destroy_order[9] == (12, 8)
+    assert destroy_order[19] == (16, 0)
+    assert destroy_order[49] == (16, 9)
+    assert destroy_order[99] == (10, 16)
+    assert destroy_order[198] == (9, 6)
+    assert destroy_order[199] == (8, 2)
+    assert destroy_order[200] == (10, 9)
+    assert destroy_order[298] == (11, 1)
+
+if __name__ == '__main__':
+    for symbol in dir():
+        if re.search('^test_', symbol):
+            print(f"Running {symbol}()")
             globals()[symbol]()
     print("Done")
