ee22d4a8e4493d84774fd062459cf4baaa0d1c9e
Eric Colton <ericcolton@gmail.com>
2021-12-14 09:17:19 -0500

    finished part 2

--- 2021/aoc_2021_14/aoc_2021_14.py
+++ 2021/aoc_2021_14/aoc_2021_14.py
@@ -9,10 +9,15 @@ Solution by Eric Colton
 """
 
 import re
-from collections import Counter
+from collections import defaultdict
 from typing import List, Dict, Tuple, Set, Optional
 
-def parse_input_data(raw_input: List[str]) -> Tuple[str, Dict[str, str]]:
+def parse_input_data(raw_input: List[str]) -> Tuple[Dict[str, int], Dict[str, str], str]:
+    data = defaultdict(int)
+    data_line = raw_input[0].rstrip()
+    for i in range(len(data_line) - 1):
+        data[data_line[i:i+2]] += 1
+
     pairs = {}
     for line in raw_input[2:]:
         match = re.fullmatch(r'(\w\w) -> (\w)', line.rstrip())
@@ -20,45 +25,45 @@ def parse_input_data(raw_input: List[str]) -> Tuple[str, Dict[str, str]]:
             pairs[match.group(1)] = match.group(2)
         else:
             raise Exception(f"Could not parse pair input line: '{line}'")
-    return (raw_input[0].rstrip(), pairs)
+    return (data, pairs, data_line[-1])
 
-def execute_insertion_round(input: str, pairs: Dict[str, str]) -> None:
-    output = []
-    for i in range(len(input) - 1):
-        pair = input[i:i+2]
+def execute_insertion_round(data: Dict[str, int], pairs: Dict[str, str]) -> Dict[str, int]:
+    new_data = defaultdict(int)
+    for pair, count in data.items():
         if pair in pairs:
-            output.append(input[i] + pairs[pair])
+            new_char = pairs[pair]
+            new_data[pair[0] + new_char] += count
+            new_data[new_char + pair[1]] += count
         else:
             raise Exception(f"Unexpected pair: '{pair}'")
-    return ''.join(output) + input[-1]
+    return new_data
 
-def execute_insertion_rounds(data: str, pairs: Dict[str, str], rounds: int) -> None:
+def execute_insertion_rounds(data: Dict[str, int], pairs: Dict[str, str], rounds: int) -> Dict[str, int]:
     for i in range(rounds):
         data = execute_insertion_round(data, pairs)
     return data
 
-def find_min_max_counts(data: str) -> Dict[str, int]:
-    min_seen, max_seen = None, None
-    min_index, max_index = None, None
-    for key, count in Counter(data).items():
-        if min_seen == None or count < min_seen:
-            min_seen = count
-            min_index = key
-        if max_seen == None or count > max_seen:
-            max_seen = count
-            max_index = key
-    return min_seen, max_seen
+def find_min_max_counts(data: Dict[str, int], last_char: str) -> Tuple[int, int]:
+    counts = defaultdict(int)
+    for pair, count in data.items():
+        counts[pair[0]] += count
+    counts[last_char] += 1
+    return min(counts.values()), max(counts.values())
 
 if __name__ == '__main__':
     input_filename = __file__.rstrip('.py') + '_input.txt'
     with open(input_filename, 'r') as file:
         raw_input = file.readlines()
-        input, pairs = parse_input_data(raw_input)
-        data = execute_insertion_rounds(input, pairs, 10)
-        min_count, max_count = find_min_max_counts(data)
+        data, pairs, last_char = parse_input_data(raw_input)
+        data = execute_insertion_rounds(data, pairs, 10)
+        min_count, max_count = find_min_max_counts(data, last_char)
         part_1 = max_count - min_count
         assert part_1 == 2657
         print(f"The solution to Part 1 is {part_1}")
 
-
+        data = execute_insertion_rounds(data, pairs, 30)
+        min_count, max_count = find_min_max_counts(data, last_char)
+        part_2 = max_count - min_count
+        assert part_2 == 2911561572630
+        print(f"The solution to Part 2 is {part_2}")
 
--- 2021/aoc_2021_14/test_aoc_2021_14.py
+++ 2021/aoc_2021_14/test_aoc_2021_14.py
@@ -23,11 +23,18 @@ CC -> N
 CN -> C""".split("\n")
 
 def test_run_10_rounds():
-    input, pairs = parse_input_data(TEST_INPUT)
-    data = execute_insertion_rounds(input, pairs, 10)
-    min_count, max_count = find_min_max_counts(data)
+    data, pairs, last_char = parse_input_data(TEST_INPUT)
+    data = execute_insertion_rounds(data, pairs, 10)
+    min_count, max_count = find_min_max_counts(data, last_char)
     diff = max_count - min_count
     assert diff == 1588
+
+def test_run_40_rounds():
+    data, pairs, last_char = parse_input_data(TEST_INPUT)
+    data = execute_insertion_rounds(data, pairs, 40)
+    min_count, max_count = find_min_max_counts(data, last_char)
+    diff = max_count - min_count
+    assert diff == 2188189693529
 
 if __name__ == '__main__':
     for symbol in dir():
