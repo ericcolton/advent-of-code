89f2b1430184a66ac3651b24303fa2b383c15f55
Eric Colton <ericcolton@gmail.com>
2022-12-19 07:05:12 -0500

    update 2022 16

--- 2022/aoc_2022_16/aoc_2022_16.py
+++ 2022/aoc_2022_16/aoc_2022_16.py
@@ -14,21 +14,24 @@ import time
 from typing import List, Dict, Tuple
 from collections import namedtuple
 
-Node = namedtuple("Node", ['name', 'rate', 'edges'])
+Node = namedtuple("Node", ['id', 'name', 'rate', 'edges'])
 
-def parse_line(line: str) -> Tuple[str, Node]:
+def parse_line(id: int, line: str) -> Tuple[str, Node]:
     match = re.match(r'Valve (\w\w) has flow rate=(\d+); tunnels? leads? to valves? ([\w\s,]+)', line)
     if not match:
         raise Exception("Unexpected line")
     edges = match.group(3).split(", ")
-    return Node(match.group(1), int(match.group(2)), edges)
+    return Node(id, match.group(1), int(match.group(2)), edges)
 
 def parse_input_data(raw_lines: List[str]) -> List[Dict[str, Node]]:
-    node_lookup = {}
+    name_lookup, id_lookup = {}, {}
+    id = 0
     for line in raw_lines:
-        node = parse_line(line.rstrip())
-        node_lookup[node.name] = node
-    return node_lookup
+        node = parse_line(id, line.rstrip())
+        name_lookup[node.name] = node
+        id_lookup[id] = node        
+        id += 1
+    return name_lookup, id_lookup
 
 def find_max_pressure(node_lookup, name, moves, turned_on, dp):
     if moves == 0:
@@ -47,61 +50,61 @@ def find_max_pressure(node_lookup, name, moves, turned_on, dp):
         dp[key] = best
     return dp[key]
 
-def find_max_pressure_with_elephant(node_lookup, me, elephant, turn, moves, turned_on, me_seen, elephant_seen, elephant_avoid, begin, dp):
-    if begin == -1:
-        begin = time.time()
-    if moves == 0:
-        return 0
-    key = me, elephant, turn, turned_on, moves
-    if key not in dp:
-        best = 0        
-        if turn == 0:
-            node = node_lookup[me]
-            elephant_avoid = set()
-            for edge in node.edges:
-                if edge not in me_seen:
-                    me_seen.add(edge)
-                    if me == elephant:
-                        elephant_avoid.add(edge)
-                    best = max(best, find_max_pressure_with_elephant(node_lookup, edge, elephant, 1, moves, turned_on, me_seen, elephant_seen, elephant_avoid, begin, dp))
-                    me_seen.remove(edge)
-            if node.rate > 0 and me not in turned_on:
-                new_on = set(turned_on)
-                new_on.add(me)
-                new_on = frozenset(new_on)
-                best = max(best, ((moves - 1) * node.rate) + find_max_pressure_with_elephant(node_lookup, me, elephant, 1, moves, new_on, set(), elephant_seen, set(), begin, dp))
-        else:
-            node = node_lookup[elephant]
-            for edge in node.edges:
-                if edge not in elephant_seen:
-                    if elephant not in elephant_avoid:
-                        elephant_seen.add(edge)
-                        best = max(best, find_max_pressure_with_elephant(node_lookup, me, edge, 0, moves - 1, turned_on, me_seen, elephant_seen, set(), begin, dp))
-                        elephant_seen.remove(edge)
-            if node.rate > 0 and elephant not in turned_on:
-                new_on =  set(turned_on)
-                new_on.add(elephant)
-                new_on = frozenset(new_on)
-                best = max(best, ((moves - 1) * node.rate) + find_max_pressure_with_elephant(node_lookup, me, elephant, 0, moves - 1, new_on, me_seen, set(), set(), begin, dp))
-        dp[key] = best
-        len_dp_keys = len(dp.keys())
-        if len_dp_keys % 1000000 == 0:
-            print(f"dp keys = {len_dp_keys} [{time.time() - begin}]")
-    return dp[key]
+class WithElephant:
+    def __init__(self, name_lookup, id_lookup):
+        self.name_lookup = name_lookup
+        self.id_lookup = id_lookup
+        id_bitmap = {}
+        i = 0
+        for node in self.name_lookup.values():
+            if node.rate > 0:
+                id_bitmap[node.id] = 0x1 << i
+                i += 1
+        self.id_bitmap = id_bitmap
+        self.dp = {}
+        self.begin = time.time()
+
+    def find_max_pressure_with_elephant(self, me, elephant, turn, moves, turned_on):
+        if moves == 0:
+            return 0
+        key = me << 28 | elephant << 22 | turn << 21 | moves << 16 | turned_on << 0
+        #key = me, elephant, turn, moves, turned_on
+        if key not in self.dp:
+            best = 0        
+            if turn == 0:
+                node = self.id_lookup[me]
+                for edge in node.edges:
+                    best = max(best, self.find_max_pressure_with_elephant(self.name_lookup[edge].id, elephant, 1, moves, turned_on))
+                if node.rate > 0 and not self.id_bitmap[me] & turned_on:
+                    new_on = turned_on | self.id_bitmap[me]
+                    best = max(best, ((moves - 1) * node.rate) + self.find_max_pressure_with_elephant(me, elephant, 1, moves, new_on))
+            else:
+                node = self.id_lookup[elephant]
+                for edge in node.edges:
+                    best = max(best, self.find_max_pressure_with_elephant(me, self.name_lookup[edge].id, 0, moves - 1, turned_on))
+                if node.rate > 0 and not self.id_bitmap[elephant] & turned_on:
+                    new_on = turned_on | self.id_bitmap[elephant]
+                    best = max(best, ((moves - 1) * node.rate) + self.find_max_pressure_with_elephant(me, elephant, 0, moves - 1, new_on))
+            self.dp[key] = best
+            len_dp_keys = len(self.dp.keys())
+            if len_dp_keys % 1_000_000 == 0:
+                print(f"dp keys = {len_dp_keys // 1_000_000} [{time.time() - self.begin}]")
+        return self.dp[key]
 
 
 if __name__ == '__main__':
     input_filename = __file__.rstrip('.py') + '_input.txt'
     with open(input_filename, 'r') as file:
         raw_input = file.readlines()
-        node_lookup = parse_input_data(raw_input)
-        part_1 = find_max_pressure(node_lookup, 'AA', 30, frozenset(), {})
-        assert part_1 == 2124
-        print(f"The solution to Part 1 is {part_1}")
+        name_lookup, id_lookup = parse_input_data(raw_input)
+        # part_1 = find_max_pressure(node_lookup, 'AA', 30, frozenset(), {})
+        # assert part_1 == 2124
+        # print(f"The solution to Part 1 is {part_1}")
 
         #part_2 = find_max_pressure_with_elephant(node_lookup, 'AA', 'AA', 0, 26, frozenset(), set(), set(), {})
         begin_time = time.time()
-        part_2 = find_max_pressure_with_elephant(node_lookup, 'AA', 'AA', 0, 26, frozenset(), set('AA'), set('AA'), set(), -1, {})
+        game = WithElephant(name_lookup, id_lookup)
+        part_2 = game.find_max_pressure_with_elephant(name_lookup['AA'].id, name_lookup['AA'].id, 0, 26, 0)
         elapsed = time.time() - begin_time
         print(f"The solution to Part 2 is {part_2} ({elapsed})")
         assert part_1 == 2124
--- 2022/aoc_2022_16/test_aoc_2022_16.py
+++ 2022/aoc_2022_16/test_aoc_2022_16.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import re
-from aoc_2022_16 import parse_input_data, find_max_pressure, find_max_pressure_with_elephant
+from aoc_2022_16 import parse_input_data, find_max_pressure, WithElephant
 
 TEST_INPUT = """Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
 Valve BB has flow rate=13; tunnels lead to valves CC, AA
@@ -15,13 +15,14 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II""".split("\n")
 
 def test_max_pressure():
-    node_lookup = parse_input_data(TEST_INPUT)
+    node_lookup, _ = parse_input_data(TEST_INPUT)
     max_pressure = find_max_pressure(node_lookup, 'AA', 30, frozenset(), {})
     assert max_pressure == 1651
 
 def test_max_pressure_with_elephant():
-    node_lookup = parse_input_data(TEST_INPUT)
-    max_pressure = find_max_pressure_with_elephant(node_lookup, 'AA', 'AA', 0, 26, frozenset(), set(), set(), {})
+    name_lookup, id_lookup = parse_input_data(TEST_INPUT)
+    with_elephant = WithElephant(name_lookup, id_lookup)
+    max_pressure = with_elephant.find_max_pressure_with_elephant(name_lookup['AA'].id, name_lookup['AA'].id, 0, 26, 0)
     assert max_pressure == 1707
 
 if __name__ == '__main__':
