52144a0266af412fd8c55dd70876747a034e29e9
Eric Colton <ericcolton@gmail.com>
2023-12-07 19:41:43 -0500

    complete aoc 2023 07

--- 2023/aoc_2023_07/aoc_2023_07.py
+++ 2023/aoc_2023_07/aoc_2023_07.py
@@ -11,9 +11,9 @@ Solution by Eric Colton
 import re
 from functools import cmp_to_key
 from collections import Counter
-from typing import Set, Dict, List
+from typing import List
 
-card_rank = {"2": 2,
+card_rank_classic = {"2": 2,
              "3": 3,
              "4": 4,
              "5": 5,
@@ -28,30 +28,17 @@ card_rank = {"2": 2,
              "A": 14,                                     
              }
 
-def hand_type(hand):
+card_rank_joker = card_rank_classic.copy()
+card_rank_joker["J"] = 1
+
+def hand_type(hand, use_jokers: bool):
     counter = Counter(hand)
-    counts = sorted(counter.values(), reverse=True)
-    if len(counts) == 1:
-        return 7 # five of a kind
-    elif len(counts) == 2:
-        if counts[0] == 4:
-            return 6 # four of a kind
-        else:
-            return 5 #full house
-    elif len(counts) == 3:
-        if counts[0] == 3:
-            return 4 # three of a kind
-        else:
-            return 3 # two pair
-    elif len(counts) == 4:
-        return 2 # two of a kind
+    if use_jokers:
+        j_count = counter["J"] if 'J' in counter else 0
+        del counter["J"]
     else:
-        return 1 # high card
-    
-def hand_type_with_jokers(hand):
-    counter = Counter(hand)
-    j_count = counter["J"] if 'J' in counter else 0
-    del counter["J"]
+        j_count = 0
+
     counts = sorted(counter.values(), reverse=True)
     if len(counts) <= 1:
         return 7 # five of a kind
@@ -70,40 +57,38 @@ def hand_type_with_jokers(hand):
     else:
         return 1 # high card
     
-def secondary_cmp(hand_a, hand_b):
+def secondary_cmp(hand_a, hand_b, ranker):
     for i in range(5):
-        if card_rank[hand_a[i]] < card_rank[hand_b[i]]:
+        if ranker[hand_a[i]] < ranker[hand_b[i]]:
             return -1
-        if card_rank[hand_a[i]] > card_rank[hand_b[i]]:
+        if ranker[hand_a[i]] > ranker[hand_b[i]]:
             return 1
     assert(False)
 
-def camel_comparator(x, y):
+def classic_comparator(x, y):
     a, b = x[0], y[0]
-    a_hand_type, b_hand_type = hand_type(a), hand_type(b)
+    a_hand_type, b_hand_type = hand_type(a, False), hand_type(b, False)
     if a_hand_type == b_hand_type:
-        return secondary_cmp(a, b)
+        return secondary_cmp(a, b, card_rank_classic)
     return a_hand_type - b_hand_type
 
 def joker_comparator(x, y):
     a, b = x[0], y[0]
-    a_hand_type, b_hand_type = hand_type_with_jokers(a), hand_type_with_jokers(b)
+    a_hand_type, b_hand_type = hand_type(a, True), hand_type(b, True)
     if a_hand_type == b_hand_type:
-        return secondary_cmp(a, b)
+        return secondary_cmp(a, b, card_rank_joker)
     return a_hand_type - b_hand_type
     
-def find_sum_of_winnings(data, use_jokers):
-    comparator = joker_comparator if use_jokers else camel_comparator
+def find_sum_of_winnings(data: List[tuple[str, int]], use_jokers: bool):
+    comparator = joker_comparator if use_jokers else classic_comparator
     ranked_hands = sorted(data, key=cmp_to_key(comparator))
-    for r in ranked_hands:
-        print(f"{r[0]} -> {hand_type_with_jokers(r[0])}")
     rank, total = 1, 0
     for hand, bid in ranked_hands:
         total += bid * rank
         rank += 1
     return total
 
-def parse_input_data(raw_lines: str):
+def parse_input_data(raw_lines: str) -> List[tuple[str, int]]:
     data = []
     for line in raw_lines:
         match = re.match(r'(\w\w\w\w\w) (\d+)', line.rstrip())
@@ -120,5 +105,5 @@ if __name__ == '__main__':
         print(f"The solution to Part 1 is {part_1}")
 
         part_2 = find_sum_of_winnings(data, True)
-        # assert part_2 == 45647654
+        assert part_2 == 251824095
         print(f"The solution to Part 2 is {part_2}")
