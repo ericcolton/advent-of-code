05025befdbe4e0b00e34216d77689392c259a36a
Eric Colton <ericcolton@gmail.com>
2023-12-16 07:26:19 -0500

    finished solution 2023_08

--- 2023/aoc_2023_08/aoc_2023_08.py
+++ 2023/aoc_2023_08/aoc_2023_08.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
- Advent of Code 2023 Day 8: Haunted Wasteland
+Advent of Code 2023 Day 8: Haunted Wasteland
 
 https://adventofcode.com/2023/day/8
 
@@ -9,88 +9,66 @@ Solution by Eric Colton
 """
 
 import re
-import math
 from typing import Dict
+from math import gcd
+from functools import reduce
 
-def count_simple_steps(data: tuple[str, Dict[str, tuple[str, str]]]) -> int:
-    instrs, lookup = data
-    count = 0
-    node = 'AAA'
-    while node != 'ZZZ':
-        dir = 0 if instrs[count % len(instrs)] == 'L' else 1
-        node = lookup[node][dir]
-        count += 1
-    return count
+def lcm(args):
+   return reduce(lambda a, b: a * b // gcd(a, b), args)
 
-def count_simultaneous_steps_HACK(data: tuple[str, Dict[str, tuple[str, str]]]) -> int:
-    instrs, lookup = data
-    count = 0
-    nodes = []
-    for key in lookup.keys():
-        if key[2] == 'A':
-            nodes.append(key)
-    memo = set()
-    empty = False
-    ends = []
-    while not empty:
-        empty = True
-        for n in range(len(nodes)):
-            node = nodes[n]
-            if node == None:
-                continue
-            empty = False
-            i = count % len(instrs)
-            key = n, node, i
-            if node[2] == 'Z':
-                print(f"{n} {node} ({i}) found at {count}")        
-            if key not in memo:
-                dir = 0 if instrs[i] == 'L' else 1
-                node = lookup[node][dir]
-                nodes[n] = node
-                memo.add(key)
-            else:
-                print(f"*** {n} exit {node} ({i}) found at {count}")
-                ends.append(count)
-                nodes[n] = None
-        count += 1
-    print(ends)
-    print(math.lcm(*ends))
-    return count
+def find_loop_length(node: str, data: tuple[str, Dict[str, tuple[str, str]]]):
+   instrs, lookup = data
+   start_node = node
+   slow, fast = start_node, start_node
+   slow_count, fast_count = 0, 0
+   # find where slow and fast intersect
+   while slow_count == 0 or slow != fast or (slow_count % len(instrs) != fast_count % len(instrs)):
+        # advance slow
+        dir = 0 if instrs[slow_count % len(instrs)] == 'L' else 1
+        slow = lookup[slow][dir]
+        slow_count += 1
+
+        #advance fast
+        for _ in range(2):
+            dir = 0 if instrs[fast_count % len(instrs)] == 'L' else 1
+            fast = lookup[fast][dir]
+            fast_count += 1
+   return slow_count
 
 def count_simultaneous_steps(data: tuple[str, Dict[str, tuple[str, str]]]) -> int:
     instrs, lookup = data
-    count = 0
-    nodes = []
-    for key in lookup.keys():
-        if key[2] == 'A':
-            nodes.append(key)
-    while not reached_end_state(nodes):
-        dir = 0 if instrs[count % len(instrs)] == 'L' else 1
-        for i in range(len(nodes)):
-            nodes[i] = lookup[nodes[i]][dir]
-        count += 1
-    return count
+    return lcm(list(map(lambda n: find_loop_length(n, data), filter(lambda a: a[2] == 'A', lookup.keys()))))
+
+def count_simple_steps(data: tuple[str, Dict[str, tuple[str, str]]]) -> int:
+   instrs, lookup = data
+   count = 0
+   node = 'AAA'
+   while node != 'ZZZ':
+       dir = 0 if instrs[count % len(instrs)] == 'L' else 1
+       node = lookup[node][dir]
+       count += 1
+   return count
 
 def parse_input_data(raw_lines: str) -> tuple[str, Dict[str, tuple[str, str]]]:
-    instrs = raw_lines[0].rstrip()
-    lookup = {}
-    for i in range(2, len(raw_lines)):
-        match = re.match(r'(\w\w\w) = \((\w\w\w), (\w\w\w)\)', raw_lines[i].rstrip())
-        if match:
-            lookup[match.group(1)] = (match.group(2), match.group(3))
-        else:
-            raise Exception("Unable to parse mapping")
-    return (instrs, lookup)
+   instrs = raw_lines[0].rstrip()
+   lookup = {}
+   for i in range(2, len(raw_lines)):
+       match = re.match(r'(\w\w\w) = \((\w\w\w), (\w\w\w)\)', raw_lines[i].rstrip())
+       if match:
+           lookup[match.group(1)] = (match.group(2), match.group(3))
+       else:
+           raise Exception("Unable to parse mapping")
+   return (instrs, lookup)
 
 if __name__ == '__main__':
-   input_filename = __file__.strip('.py') + '_input.txt'
-   with open(input_filename, 'r') as file:
-        raw_input = file.readlines()
-        data = parse_input_data(raw_input)
-        part_1 = count_simple_steps(data)
-        assert part_1 == 16579
-        print(f"The solution to Part 1 is {part_1}")
+  input_filename = __file__.rstrip('.py') + '_input.txt'
+  with open(input_filename, 'r') as file:
+       raw_input = file.readlines()
+       data = parse_input_data(raw_input)
+       part_1 = count_simple_steps(data)
+       assert part_1 == 16579
+       print(f"The solution to Part 1 is {part_1}")
 
-        part_2 = count_simultaneous_steps_HACK(data)
-        #assert part_2 == 16579
-        print(f"The solution to Part 2 is {part_2}")
+       part_2 = count_simultaneous_steps(data)
+       assert part_2 == 12927600769609
+       print(f"The solution to Part 2 is {part_2}")
--- 2023/aoc_2023_08/test_aoc_2023_08.py
+++ 2023/aoc_2023_08/test_aoc_2023_08.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import re
-from typing import Dict
-#from aoc_2023_08 import parse_input_data, 
+from aoc_2023_08 import parse_input_data, count_simple_steps, count_simultaneous_steps
 
 TEST_INPUT_1 = """RL
 
@@ -31,66 +30,20 @@ TEST_INPUT_3 = """LR
 22Z = (22B, 22B)
 XXX = (XXX, XXX)""".split("\n")
 
-def reached_end_state(nodes):
-    for n in nodes:
-        if n[2] != 'Z':
-            return False
-    return True
-
-def count_simultaneous_steps(data: tuple[str, Dict[str, tuple[str, str]]]) -> int:
-    instrs, lookup = data
-    count = 0
-    nodes = []
-    for key in lookup.keys():
-        if key[2] == 'A':
-            nodes.append(key)
-    while not reached_end_state(nodes):
-        dir = 0 if instrs[count % len(instrs)] == 'L' else 1
-        for i in range(len(nodes)):
-            nodes[i] = lookup[nodes[i]][dir]
-        count += 1
-    return count
-
-def count_total_steps(data: tuple[str, Dict[str, tuple[str, str]]]) -> int:
-    instrs, lookup = data
-    count = 0
-    node = 'AAA'
-    while node != 'ZZZ':
-        dir = 0 if instrs[count % len(instrs)] == 'L' else 1
-        node = lookup[node][dir]
-        count += 1
-    return count
-
-def parse_input_data(raw_lines: str) -> tuple[str, Dict[str, tuple[str, str]]]:
-    instrs = raw_lines[0].rstrip()
-    lookup = {}
-    for i in range(2, len(raw_lines)):
-        match = re.match(r'(\w\w\w) = \((\w\w\w), (\w\w\w)\)', raw_lines[i].rstrip())
-        if match:
-            lookup[match.group(1)] = (match.group(2), match.group(3))
-        else:
-            raise Exception("Unable to parse mapping")
-    return (instrs, lookup)
-
 def test_sum_of_steps_one():
     data = parse_input_data(TEST_INPUT_1)
-    rv = count_total_steps(data)
+    rv = count_simple_steps(data)
     assert(rv == 2)
 
 def test_sum_of_steps_two():
     data = parse_input_data(TEST_INPUT_2)
-    rv = count_total_steps(data)
+    rv = count_simple_steps(data)
     assert(rv == 6)    
 
 def test_sum_of_simultaneous_steps():
     data = parse_input_data(TEST_INPUT_3)
     rv = count_simultaneous_steps(data)
     assert(rv == 6)
-
-# def test_sum_of_winnings_with_jokers():
-#     data = parse_input_data(TEST_INPUT)
-#     part_2 = find_sum_of_winnings(data, True)
-#     assert(part_2 == 5905)
 
 if __name__ == '__main__':
     for symbol in dir():
